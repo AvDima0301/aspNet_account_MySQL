e7fb1b4e12b8cbee51c9e1f8c1321392f00b4df4
novakvova <novakvova@gmail.com>
2022-04-02 10:07:51 +0300

    Add Account Corntroller begin register

---
+++ News.Web/News.Web/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using News.Web.Data.Entities.Identity;
+using News.Web.Models;
+
+namespace News.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterViewModel model)
+        {
+            return Ok(new { token="Сало" });
+        }
+    }
+}
