95843a0daebdeeb18a9f77ddadfbed592337d0d4
novakvova <novakvova@gmail.com>
2022-04-16 10:22:50 +0300

    Is good show list users

--- Android/app/src/main/java/com/example/example/constants/Urls.java
+++ Android/app/src/main/java/com/example/example/constants/Urls.java
@@ -1,6 +1,6 @@
 package com.example.example.constants;
 
 public class Urls {
-    public static final String BASE = "http://10.0.2.2:5126";
-    //public static final String BASE = "https://myrenta.tk";
+    //public static final String BASE = "http://10.0.2.2:5126";
+    public static final String BASE = "https://spu911.novakvova.com";
 }
